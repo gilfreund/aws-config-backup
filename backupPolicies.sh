99f9e8f6136e5ada0afe5e07e846a3f7977a9c8c
Gil Freund <gilf@sysnet.co.il>
2019-11-20 19:17:55 +0000

    Update policy and role backup to use the mksubdir function

--- backupPolicies.sh
+++ backupPolicies.sh
@@ -1,19 +1,23 @@
-#!/bin/bash -x
+#!/bin/bash
 RCFILE="$(dirname $(readlink -f $0))/backup.rc"
 if [[ -f "$RCFILE" ]] ; then
 	        source "$RCFILE"
 fi
 
+SUBDIR=$(mksubdir Policies)
+
 $AWSCMD iam list-policies --scope Local --only-attached | awk '{print $2" "$7" "$10}' | while read -r arn path name ; do
-if [[ ! -d ${WORKDIR}/policies${path} ]]; then
-	mkdir -p ${WORKDIR}/policies${path}
+if [[ ! -d ${SUBDIR}${path} ]]; then
+	mkdir -p ${SUBDIR}${path}
 fi
-$AWSGET iam get-policy --policy-arn $arn > ${WORKDIR}/policies${path}${name}.json
-$AWSGET iam list-entities-for-policy --policy-arn $arn > ${WORKDIR}/policies${path}${name}-entities.json
+$AWSGET iam get-policy --policy-arn ${arn} > ${SUBDIR}${path}${name}.json
+$AWSGET iam list-entities-for-policy --policy-arn ${arn} > ${SUBDIR}${path}${name}-entities.json
 MINVERSION="$($AWSCMD iam list-policy-versions --policy-arn ${arn} | awk '{print $4}' | cut -c2- | sort | head -1)"
 MAXVERSION="$($AWSCMD iam list-policy-versions --policy-arn ${arn} | awk '{print $4}' | cut -c2- | sort | tail -1)" 
 for (( version=$MINVERSION; version<=$MAXVERSION; version++ )) ; do
-	$AWSGET iam get-policy-version --policy-arn $arn --version-id $version > ${WORKDIR}/policies${path}${name}-v${version}.json
+	if [[ ! -f ${SUBDIR}${path}${name}-v${version}.json ]] ; then
+		$AWSGET iam get-policy-version --policy-arn ${arn} --version-id v${version} > ${SUBDIR}${path}${name}-v${version}.json
+	fi
 done
 done
 
--- backupRoles.sh
+++ backupRoles.sh
@@ -4,22 +4,24 @@ if [[ -f "$RCFILE" ]] ; then
 	        source "$RCFILE"
 fi
 
+SUBDIR=$(mksubdir roles)
+
 $AWSCMD  iam  list-roles | grep ROLES | grep -v aws-service-role | awk '{print $2}' | awk -F":" '{print $NF}' | awk -F"/" '{print $2" "$NF}' | while read -r Path RoleName ; do
-$AWSCMD iam list-role-policies --role-name $RoleName | awk '{print $2}' | while read -r PolicyNames ; do
-	if [[ "$Path" != "$RoleName" ]] ; then
-		if [[ ! -d "$Path" ]] ; then
-			mkdir "$Path"
+$AWSCMD iam list-role-policies --role-name ${RoleName} | awk '{print $2}' | while read -r PolicyNames ; do
+	if [[ "$Path" != "${RoleName}" ]] ; then
+		if [[ ! -d "${SUBDIR}/${Path}" ]] ; then
+			mkdir "${SUBDIR}/${Path}"
 		fi
-		$AWSGET iam get-role --role-name $RoleName > $WORKDIR/roles/$Path/$RoleName.json
-		$AWSGET iam list-attached-role-policies --role-name $RoleName > $WORKDIR/roles/$Path/$RoleName-attached-policies.json
-		if [[ -n $PolicyNames ]] ; then
-			$AWSGET iam get-role-policy --role-name $RoleName --policy-name $PolicyNames > $WORKDIR/roles/$RoleName-role-policies.json
+		$AWSGET iam get-role --role-name ${RoleName} > ${SUBDIR}/${Path}/${RoleName}.json
+		$AWSGET iam list-attached-role-policies --role-name ${RoleName} > ${SUBDIR}/${Path}/${RoleName}-attached-policies.json
+		if [[ -n "${PolicyNames}" ]] ; then
+			$AWSGET iam get-role-policy --role-name ${RoleName} --policy-name ${PolicyNames} > ${SUBDIR}/${RoleName}-role-policies.json
 		fi
 	fi
-	$AWSGET iam get-role --role-name $RoleName > $WORKDIR/roles/$RoleName.json
-	$AWSGET iam list-attached-role-policies --role-name $RoleName > $WORKDIR/roles/$RoleName-attached-policies.json
-	if [[ -n $PolicyNames ]] ; then
-		$AWSGET iam get-role-policy --role-name $RoleName --policy-name $PolicyNames > $WORKDIR/roles/$RoleName-role-policies.json
+	$AWSGET iam get-role --role-name ${RoleName} > ${SUBDIR}/${RoleName}.json
+	$AWSGET iam list-attached-role-policies --role-name ${RoleName} > ${SUBDIR}/${RoleName}-attached-policies.json
+	if [[ -n ${PolicyNames} ]] ; then
+		$AWSGET iam get-role-policy --role-name ${RoleName} --policy-name ${PolicyNames} > ${SUBDIR}/${RoleName}-role-policies.json
 	fi
 done
 done
