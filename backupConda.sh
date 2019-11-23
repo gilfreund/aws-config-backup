d8c30ece23cb15a161f74cecd66d565780cfe17e
Gil Freund <gilf@sysnet.co.il>
2019-11-23 14:57:32 +0000

    Added Conda backup

---
+++ backupConda.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+RCFILE="$(dirname $(readlink -f $0))/backup.rc"
+if [[ -f "$RCFILE" ]] ; then
+	        source "$RCFILE"
+fi
+
+if [[ -z CONDA_ROOT ]] ; then
+	echo "CONDA_ROOT is not defined in backup.user.rc"
+	echo "Cannot backup conda"
+	exit 1
+fi
+
+ACTIVATE="$CONDA_ROOT/bin/activate"
+if [ -z $CONDA_EXE ]
+then
+	if [ -f $ACTIVATE ]
+	then
+		source $ACTIVATE
+	else
+		echo "can't find $ACTIVATE" 
+		exit 1
+	fi
+fi
+
+SUBDIR=$(mksubdir .)
+for env in $(conda env list | tail  --lines=+3 | awk '{print $1}')
+do 
+	echo Processing $env
+	echo -e "\tExport environenment"
+	conda env export --name $env > ${SUBDIR}/${env}.yaml
+	echo -e "\tExport specs"
+	conda list --explicit --name ${env} > ${SUBDIR}/${env}-dependencies.txt
+	HISTORY="${CONDA_ROOT}/envs/${env}/conda-meta/history"
+	if [[ -f $HISTORY ]] ; then
+		echo -e "\tCopy history"
+		cp $HISTORY ${SUBDIR}/${env}-history.txt
+	fi
+done
+
+commit conda Configurations
