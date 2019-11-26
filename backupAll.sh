60f41b0563060b19bcf0c9fc7f82000c2b4588d6
Gil Freund <gilf@sysnet.co.il>
2019-11-26 11:25:03 +0000

    Added Lambda and run-all command

---
+++ backupAll.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Run all backup scripts
+for script in backup*.sh ; do 
+	# excet myself...
+	if [[ $script != ${0##*/} ]] ; then 
+		echo "running $script"
+		$(dirname "$(realpath $script)")
+	fi
+done
