6835740522bdae11f200adb72080196d730040d4
Gil Freund <gilf@sysnet.co.il>
2019-11-27 17:51:00 +0000

    Added a master script to run all backup actions

--- backupAll.sh
+++ backupAll.sh
@@ -5,6 +5,6 @@ for script in backup*.sh ; do
 	# excet myself...
 	if [[ $script != ${0##*/} ]] ; then 
 		echo "running $script"
-		$(dirname "$(realpath $script)")
+		$(dirname "$(realpath $script)")/$script
 	fi
 done
