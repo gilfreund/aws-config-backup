b48e552666166a47ee4a5780a284f039ff274ded
Gil Freund <gilf@sysnet.co.il>
2019-12-05 20:54:58 +0000

    Added checkForValues function

--- backupRoles.sh
+++ backupRoles.sh
@@ -12,26 +12,36 @@ fi
 
 echo "Backing up IAM Roles configurations"
 
+function checkForValues {
+	local result=$($AWSCMD iam $1 --role-name $RoleName | awk -F '\t' {'print $2'})
+	if [[ -n "${result}" ]] ; then
+		if [[ -n "${2}" ]] ; then 
+			$AWSGET iam $1 --role-name $RoleName > ${TARGET_FILE}-$2.json
+		else
+			echo "true"
+		fi
+	fi
+}
+
 SUBDIR=$(mksubdir roles)
 
-$AWSCMD  iam  list-roles | grep ROLES | grep -v aws-service-role | awk '{print $2}' | awk -F":" '{print $NF}' | awk -F"/" '{print $2" "$NF}' | while read -r Path RoleName ; do
-$AWSCMD iam list-role-policies --role-name ${RoleName} | awk '{print $2}' | while read -r PolicyNames ; do
-	if [[ "$Path" != "${RoleName}" ]] ; then
-		if [[ ! -d "${SUBDIR}/${Path}" ]] ; then
-			mkdir "${SUBDIR}/${Path}"
+$AWSCMD iam  list-roles | grep ROLES | grep -v aws-service-role | awk -F '\t' {'print $6" "$8'} | while read -r Path RoleName ; do
+	if [[ ${#Path} -gt 1 ]] ; then
+		if [[ ! -d ${SUBDIR}${Path} ]] ; then
+			mkdir ${SUBDIR}${Path}
 		fi
-		$AWSGET iam get-role --role-name ${RoleName} > ${SUBDIR}/${Path}/${RoleName}.json
-		$AWSGET iam list-attached-role-policies --role-name ${RoleName} > ${SUBDIR}/${Path}/${RoleName}-attached-policies.json
-		if [[ -n "${PolicyNames}" ]] ; then
-			$AWSGET iam get-role-policy --role-name ${RoleName} --policy-name ${PolicyNames} > ${SUBDIR}/${RoleName}-role-policies.json
-		fi
+		TARGET_FILE="${SUBDIR}${Path}${RoleName}"
+	else
+		TARGET_FILE="${SUBDIR}/${RoleName}"
 	fi
-	$AWSGET iam get-role --role-name ${RoleName} > ${SUBDIR}/${RoleName}.json
-	$AWSGET iam list-attached-role-policies --role-name ${RoleName} > ${SUBDIR}/${RoleName}-attached-policies.json
-	if [[ -n ${PolicyNames} ]] ; then
-		$AWSGET iam get-role-policy --role-name ${RoleName} --policy-name ${PolicyNames} > ${SUBDIR}/${RoleName}-role-policies.json
+	$AWSGET iam get-role --role-name ${RoleName} > ${TARGET_FILE}.json
+	checkForValues list-attached-role-policies attached-policies
+	checkForValues list-instance-profiles-for-role instance-profiles
+	if [[ "$(checkForValues list-role-policies)" == "true" ]] ; then 
+		$AWSCMD iam list-role-policies --role-name ${RoleName} | awk -F '\t' '{print $2}' | while read -r PolicyNames ; do
+			$AWSGET iam get-role-policy --role-name ${RoleName} --policy-name ${PolicyNames} > ${TARGET_FILE}-role-policies.json
+		done
 	fi
-done
 done
 
 commit IAM Role Configurations
