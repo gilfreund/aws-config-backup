929fe30988800b0904d66479e6088653e6922cd1
Gil Freund <gilf@sysnet.co.il>
2019-12-11 19:31:46 +0000

    Backup code and replace merge grep with awk

--- backupLambda.sh
+++ backupLambda.sh
@@ -15,18 +15,20 @@ echo "Backing up Lambda"
 SUBDIR=$(mksubdir functions)
 FEATURE=lambda
 
-$AWSCMD lambda list-functions | grep FUNCTIONS | awk -F '\t' {'print $5" "$6'} | while read -r OBJECT_NAME functionName ; do 
+$AWSCMD lambda list-functions | awk -F '\t' '/FUNCTIONS/ {print $5" "$6}' | while read -r OBJECT_NAME functionName ; do 
 	TARGET_FILE=$(buildTargetFilePrefix $functionName)
 	$AWSGET lambda get-function --function-name $OBJECT_NAME > ${TARGET_FILE}.json
 	backupIfNotNull list-tags tags
 	if [[ "$(backupIfNotNull list-versions-by-function)" == "true" ]] ; then
-		$AWSCMD lambda list-versions-by-function --function-name $OBJECT_NAME | grep VERSIONS | awk -F '\t' {'print $NF'}  | while read -r Version ; do
+		$AWSCMD lambda list-versions-by-function --function-name $OBJECT_NAME | awk -F '\t' '/VERSIONS/ {print $NF}' | while read -r Version ; do
 			$AWSCMD lambda get-function-configuration --function-name $OBJECT_NAME:$Version > $TARGET_FILE-$Version.json 
+			CODE_URL="$($AWSCMD lambda get-function --function-name $OBJECT_NAME:$Version | awk -F '\t' '/CODE/ {print $2}')"
+			curl --silent "$CODE_URL" --output ${TARGET_FILE}-${Version}.zip
 		done
 	fi
 	backupIfNotNull list-aliases aliases
 	if [[ "$(backupIfNotNull list-aliases)" == "true" ]] ; then
-		$AWSGET lambda list-aliases --function-name $OBJECT_NAME | grep ALIASES | awk -F '\t' {'print $4'} | while read -r aliases ; do
+		$AWSGET lambda list-aliases --function-name $OBJECT_NAME | awk -F '\t' '/ALIASES/ {print $4}' | while read -r aliases ; do
 			$AWSGET get-alias --function-name $OBJECT_NAME --name $aliases > $TARGET_FILE-aliases-$aliases.json
 		done
 	fi
