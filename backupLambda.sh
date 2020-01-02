7f7b95ed206c422fc02108758d1373cf2aef7243
Gil Freund <gilf@sysnet.co.il>
2020-01-02 08:52:46 +0000

    Move to query

--- backupLambda.sh
+++ backupLambda.sh
@@ -15,21 +15,23 @@ echo "Backing up Lambda"
 SUBDIR=$(mksubdir functions)
 FEATURE=lambda
 
-$AWSCMD lambda list-functions | awk -F '\t' '/FUNCTIONS/ {print $5" "$6}' | while read -r OBJECT_NAME functionName ; do 
+$AWSCMD lambda list-functions --query Functions[*].[FunctionArn,FunctionName] | while IFS=$'\t' read -r OBJECT_NAME functionName ; do 
 	TARGET_FILE=$(buildTargetFilePrefix $functionName $functionName)
 	$AWSGET lambda get-function --function-name $OBJECT_NAME > ${TARGET_FILE}.json
 	backupIfNotNull $FEATURE list-tags tags
 	if [[ "$(backupIfNotNull $FEATURE list-versions-by-function)" == "true" ]] ; then
-		$AWSCMD lambda list-versions-by-function --function-name $OBJECT_NAME | awk -F '\t' '/VERSIONS/ {print $NF}' | while read -r Version ; do
-			$AWSCMD lambda get-function-configuration --function-name $OBJECT_NAME:$Version > $TARGET_FILE-$Version.json 
-			CODE_URL="$($AWSCMD lambda get-function --function-name $OBJECT_NAME:$Version | awk -F '\t' '/CODE/ {print $2}')"
+		versions=$($AWSCMD lambda list-versions-by-function --function-name $OBJECT_NAME --query Versions[*].Version)
+		for Version in $versions ; do 
+			$AWSGET lambda get-function-configuration --function-name $OBJECT_NAME:$Version > $TARGET_FILE-"$Version".json 
+			CODE_URL="$($AWSCMD lambda get-function --function-name $OBJECT_NAME:$Version --query Code.Location)"
 			curl --silent "$CODE_URL" --output ${TARGET_FILE}-${Version}.zip
 		done
 	fi
 	backupIfNotNull $FEATURE list-aliases aliases
 	if [[ "$(backupIfNotNull $FEATURE list-aliases)" == "true" ]] ; then
-		$AWSGET lambda list-aliases --function-name $OBJECT_NAME | awk -F '\t' '/ALIASES/ {print $4}' | while read -r aliases ; do
-			$AWSGET get-alias --function-name $OBJECT_NAME --name $aliases > $TARGET_FILE-aliases-$aliases.json
+		aliases=$($AWSCMD lambda list-aliases --function-name $OBJECT_NAME --query Aliases[*].Name)
+		for alias in $aliases ; do
+			$AWSGET lambda get-alias --function-name $OBJECT_NAME --name $alias > $TARGET_FILE-aliases-$alias.json
 		done
 	fi
 done
